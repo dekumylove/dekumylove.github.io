1989547ac040af1a20eaedb8c2a186ed418f7aa0
dekumylove <60187278+dekumylove@users.noreply.github.com>
2025-04-10 22:27:03 +0800

    Update loadstar.js

--- js/loadstar.js
+++ js/loadstar.js
@@ -1,6 +1,6 @@
 const starCountLink = document.getElementById('starCountLink');
 // 使用GitHub API获取星标数量，无需认证即可获取公开项目数据
-fetch('https://api.github.com/repos/apache/superset')
+fetch('https://api.github.com/repos/apache/singa')
    .then(response => response.json())
    .then(data => {
         const starCount = data.stargazers_count;
